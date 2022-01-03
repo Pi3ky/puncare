16c8717d3c42d3af179573e2e80103fd7943256e
pieky26 <khang@leadsgen.asia>
2022-01-03 15:24:43 +0700

    [serverjs]

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
+const forceSSL = function () {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
 app.use(express.static('./dist/puncare'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/puncare/'}),
-);
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname,'/dist/puncare/index.html'));
+});
 
-// Start the app by listening on the default Heroku port
+app.use(forceSSL());
+
 app.listen(process.env.PORT || 8080);
