ae81862939595d3e2195f6e7f0d58e910058310c
Bùi Đình Khang <chocopie.nuce@gmail.com>
2022-01-07 13:18:24 +0700

    [change] url

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-// app.use(express.static('./dist/puncare'));
+app.use(express.static('./dist/puncare'));
 
 
-app.use(express.static(__dirname + '/dist'));
+// app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/puncare/'}),
