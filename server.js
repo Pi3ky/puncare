a9766c6349e30b5800761ab45aef67c2ee656f55
Bùi Đình Khang <chocopie.nuce@gmail.com>
2022-01-07 11:52:55 +0700

    [change] serverjs

--- server.js
+++ server.js
@@ -4,10 +4,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/puncare'));
+// app.use(express.static('./dist/puncare'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/puncare/'}),
-);
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/puncare/'}),
+// );
+
+app.use(express.static(__dirname + '/dist'));
 
 app.listen(process.env.PORT || 8080);
