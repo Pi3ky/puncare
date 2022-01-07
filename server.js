e3b15963ead68f18b14c2c9ace90112dbc4626ff
Bùi Đình Khang <chocopie.nuce@gmail.com>
2022-01-07 11:41:13 +0700

    [add] package

--- server.js
+++ server.js
@@ -3,23 +3,11 @@ const path = require('path');
 
 const app = express();
 
-const forceSSL = function () {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-};
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/puncare'));
 
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/puncare/'}),
 );
-
-app.use(forceSSL());
 
 app.listen(process.env.PORT || 8080);
