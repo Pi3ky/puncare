136aee993ac0454d72373e17b3cbf9b99f943108
pieky26 <khang@leadsgen.com>
2022-01-07 11:10:54 +0700

    [change] port -> 8080

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
 // const forceSSL = function () {
@@ -21,4 +22,4 @@ app.get('/*', (req, res) =>
 
 // app.use(forceSSL());
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 8080);
